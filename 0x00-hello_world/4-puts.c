894af642e95fa02e6fc34127762e84a7da784ef7
kayc0des <k.boafo@alustudent.com>
2022-10-13 13:56:22 -0700

    Puts command in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
    /*  printf() displays the string inside quotation */
-	char MyStr[] = "Programming is like building a multilingual puzzle\n";
+	char MyStr[] = "\"Programming is like building a multilingual puzzle\n";
 	puts(MyStr);
    return 0;
 }
