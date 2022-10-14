ea278dc80bd116021dad0d5173ca5281eee60817
kayc0des <k.boafo@alustudent.com>
2022-10-14 08:24:40 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - Entry point
  *
- * Return: always 0 
+ * Return: always 0
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
