b8dfbe3f3ed4766c687ce4509e5f2888d97ab43a
kayc0des <k.boafo@alustudent.com>
2022-10-20 06:33:52 -0700

    task 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -100,4 +100,8 @@ int mul(int a, int b);
  * print_numbers - print numbers from 0 to 9
  */
 void print_numbers(void);
+/**
+ * print_most_numbers - print numbers from 0 to 9 but not 2 and 4
+ */
+void print_most_numbers(void);
 #endif
