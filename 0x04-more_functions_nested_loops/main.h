ae48a12e58556d2d6e71aa12b87c2bd587dc7b76
kayc0des <k.boafo@alustudent.com>
2022-10-20 07:19:16 -0700

    task 7

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -110,7 +110,25 @@ void print_most_numbers(void);
 void more_numbers(void);
 /**
  * print_line - prints a line in the terminal
- * @n: number of times character is printe
+ * @n: number of times character is printed
  */
 void print_line(int n);
+void print_diagonal(int n);
+/**
+ * print_square - prints a square
+ *
+ * @size: prints the size of whatever
+ */
+void print_square(int size);
+/**
+ * print_triangle - prints triangle
+ * @size: size of triangle
+ */
+void print_triangle(int size);
+/**
+ * print_number - prints number
+ *
+ * @n: integer to print to char
+ */
+void print_number(int n);
 #endif
